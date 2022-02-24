2008688eec7fdf728faa1288bc85f43933f8438d
jpp2121 <78234225+jpp2121@users.noreply.github.com>
2022-02-24 13:26:55 -0500

    Initial commit

---
+++ Assets/Scripts/LookThenMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookThenMove : MonoBehaviour
+{
+    
+    public GameObject ground;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Local variables for camera, raycast, hitting, hitobject
+
+        Transform camera = Camera.main.transform;
+
+        Ray rayCast;
+
+        RaycastHit hit;
+
+        GameObject hitObj;
+
+        //Create line along ray
+        //Ray function
+        //Ray variables need to 
+        
+    }
+}
